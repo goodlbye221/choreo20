98092667fe99ed328d727c811238361e06535f5f
goodlbye221 <138499939+goodlbye221@users.noreply.github.com>
2025-03-02 20:44:38 -0800

    Update index.js

--- files/index.js
+++ files/index.js
@@ -6,8 +6,8 @@ const UUID = process.env.UUID || '342f4b2e-ca6b-4dbc-b0d9-4f43d2799475'; //若�
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '5ddVS93Eq0Uc9he880';
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'chookerto.bmfdv99gv.tk';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiYzVkNGZhYTk0OGY3YTgwNjU5YTlhYzkwYmQ3NmM0NDciLCJ0IjoiODI0YzNkY2UtZDg2NC00YTk5LTk1ZjYtZWExOWM3MDliMDFhIiwicyI6Ik4ySXdPRE13TXprdFpEQTNZaTAwTm1Jd0xUZzJOalF0TnpBM1lUVXpOV1k1TmprMCJ9';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'feell.we2o0rtrd.us.kg';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiNTM5NDkxMTZlYzA2NmFmMWY0MmVhNjI4YzEyMDcyMjkiLCJ0IjoiOTk5ZmIzYzMtZjgxMy00Zjc3LTgyOTUtOGUyNDI4ODIwN2FmIiwicyI6Ik9UaGhNekppWWpBdE9XSTFaaTAwTTJSbExXSTBORGN0TXpneU16VTBZMlkyWldZeiJ9';
 const CFIP = process.env.CFIP || 'na.ma';
 const NAME = process.env.NAME || 'Choreo';
 
